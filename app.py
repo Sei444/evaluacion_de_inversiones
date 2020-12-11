facfee5642bec06a5ca8fad22a1f9440b9c8225c
Andre Arellano Z <andre.arellano3@gmail.com>
2020-12-11 15:11:28 -0400

    ventana principal

---
+++ app.py
@@ -0,0 +1,14 @@
+import sys
+from PyQt5 import uic 
+from PyQt5.QtWidgets import QMainWindow, QApplication
+
+class principal (QMainWindow):
+  def __init__(self):
+    super().__init__()
+    uic.loadUi("ventanaP.ui",self)
+
+if __name__== '__main__':
+  app = QApplication(sys.argv)
+  ventana = principal()
+  ventana.show()
+  sys.exit(app.exec_())
